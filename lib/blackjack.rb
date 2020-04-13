cdef5a3661172d6da50f3e3ddcfff2db77978daf
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-13 04:44:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,7 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  display_card_total(deal_card + deal_card)
+  card_total = deal_card + deal_card)
+  display_card_total(card_total)
+  card_total
 end
 
 def hit?
