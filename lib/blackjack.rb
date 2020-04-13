c4a6809411a86cf74cc5942585477ba9bb2a69be
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-13 04:42:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,11 +20,12 @@ end
 
 def get_user_input
   # code #get_user_input here
-  user_input = gets.chomp 
+  gets.chomp 
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
